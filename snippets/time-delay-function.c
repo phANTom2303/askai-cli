dd9daba966585f960588830e13487ac8dd6b2022
phaANTom2303 <anikavy.goenka@gmail.com>
2025-10-03 03:07:35 +0530

    added time delay string print function to snippet

--- snippets/time-delay-function.c
+++ snippets/time-delay-function.c
@@ -21,6 +21,20 @@ void sleep_ms(long milliseconds) {
     }
 }
 
+void displayStringWithDelay(char *str) {
+    char ch;
+    int len = strlen(str);
+    int lineCount = 0;
+    for (int i = 0; i < len; i++) {
+        ch = str[i];
+        printf("%c", ch);
+        if (i % 100 == 0)
+            sleep_ms(50);
+        else if (i % 501 == 0)
+            sleep_ms(150);
+    }
+}
+
 int main() {
     for (int i = 1; i < 10; i++) {
         printf("%d\n", i);
